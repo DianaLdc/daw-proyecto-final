72ab667149a8164430624276b99ecdf4ec000f88
AMrtGllg <amartelg@unsa.edu.pe>
2026-06-28 15:48:02 -0500

    vistas CRUD, formulario, vista JSON y Ajax

---
+++ adopciones/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Animal
+
+class AnimalForm(forms.ModelForm):
+    class Meta:
+        model = Animal
+        fields = ['nombre', 'especie', 'raza', 'edad', 'foto', 'descripcion', 'estado', 'dueno']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre del animal'}),
+            'especie': forms.Select(choices=[('perro', 'Perro'), ('gato', 'Gato')], attrs={'class': 'form-select'}),
+            'raza': forms.TextInput(attrs={'class': 'form-control'}),
+            'edad': forms.NumberInput(attrs={'class': 'form-control', 'min': 0}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'estado': forms.Select(attrs={'class': 'form-select'}),
+        }
+
+    def clean_edad(self):
+        edad = self.cleaned_data.get('edad')
+        if edad < 0 or edad > 30:
+            raise forms.ValidationError("La edad debe estar entre 0 y 30 años")
+        return edad
+
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if any(char.isdigit() for char in nombre):
+            raise forms.ValidationError("El nombre no puede contener números")
+        return nombre
--- adopciones/views.py
+++ adopciones/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from .models import Animal
+from .forms import AnimalForm
+from django.http import JsonResponse
 
-# Create your views here.
+def animal_list(request):
+    animales = Animal.objects.all()
+    return render(request, 'adopciones/animal_list.html', {'animales': animales})
+
+def animal_detail(request, pk):
+    animal = get_object_or_404(Animal, pk=pk)
+    return render(request, 'adopciones/animal_detail.html', {'animal': animal})
+
+def animal_delete(request, pk):
+    animal = get_object_or_404(Animal, pk=pk)
+    if request.method == 'POST':
+        animal.delete()
+        return redirect('animal_list')
+    return render(request, 'adopciones/animal_confirm_delete.html', {'animal': animal})
+
+def animal_create(request):
+    if request.method == 'POST':
+        form = AnimalForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('animal_list')
+    else:
+        form = AnimalForm()
+    return render(request, 'adopciones/animal_form.html', {'form': form})
+
+def animal_update(request, pk):
+    animal = get_object_or_404(Animal, pk=pk)
+    if request.method == 'POST':
+        form = AnimalForm(request.POST, request.FILES, instance=animal)
+        if form.is_valid():
+            form.save()
+            return redirect('animal_list')
+    else:
+        form = AnimalForm(instance=animal)
+    return render(request, 'adopciones/animal_form.html', {'form': form})
+
+def animal_api(request):
+    animales = Animal.objects.all().values('id', 'nombre', 'especie', 'raza', 'edad', 'descripcion', 'estado')
+    return JsonResponse(list(animales), safe=False)
