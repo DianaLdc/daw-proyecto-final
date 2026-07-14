68d515a9accfaedc1754c65fe3718aa74bffd7bc
rwciANA <rcondorii@unsa.edu.pe>
2026-07-13 20:19:47 -0500

    cambio en app.css

---
+++ FRONTEND/src/pages/Tienda.jsx
@@ -0,0 +1,10 @@
+import "./Tienda.css";
+
+export default function Tienda() {
+  return (
+    <div className="tienda">
+      <h1>Tienda Virtual</h1>
+      <p>Bienvenido a la tienda de Veterinaria Aurora.</p>
+    </div>
+  );
+}
